db6a764e56b8ba5be68940256923c4fd7e0ddc30
Cenon Ebora <remote.cenon@gmail.com>
2017-12-08 19:08:32 +0800

    Adding ce-site-common library

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {FORM_DIRECTIVES} from '@angular/common';
 import {i18n} from 'app/services/i18n/i18n';
+import {HelloWorld} from 'ce-site-common/components';
 
 @Component({
   selector: 'home',
-  directives: [...FORM_DIRECTIVES],
+  directives: [...FORM_DIRECTIVES, HelloWorld],
   pipes: [],
   styles: [require('./home.style.scss')],
   template: require('./home.template.html')
