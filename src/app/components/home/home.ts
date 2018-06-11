45a011d773bbc4c0aaf04c176899153dbed294c9
Cenon Ebora <remote.cenon@gmail.com>
2018-06-12 02:00:05 +0800

    updated dist

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {FORM_DIRECTIVES} from '@angular/common';
 import {i18n} from 'app/services/i18n/i18n';
-import {HelloWorld} from 'ce-site-common/components';
 
 @Component({
   selector: 'home',
-  directives: [...FORM_DIRECTIVES, HelloWorld],
+  directives: [...FORM_DIRECTIVES],
   pipes: [],
   styles: [require('./home.style.scss')],
   template: require('./home.template.html')
