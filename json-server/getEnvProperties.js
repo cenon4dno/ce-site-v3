8bd856dcc3e4dc347a52d54a1b65b9c2f9584cce
Cenon Ebora <remote.cenon@gmail.com>
2018-06-12 03:14:56 +0800

    adding env variables

--- json-server/getEnvProperties.js
+++ json-server/getEnvProperties.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var envConfigs = require('./config');
-var _ = require('lodash');
-var env = process.env.NODE_ENV || 'dev';
+var env = process.env.V3_ENV || 'dev';
 
 //@TODO: Create a route server here doc: https://github.com/typicode/json-server
 
 module.exports = function() {
-    return _.merge(envConfigs['base'], envConfigs[env]);
+    return envConfigs[env];
 }
