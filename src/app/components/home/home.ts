8791f55fe059c31a86b3b4335d1c294f98aa65e6
Cenon Ebora <remote.cenon@gmail.com>
2018-06-18 23:36:17 +0800

    Adding common directives

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
-import {FORM_DIRECTIVES} from '@angular/common';
 import {i18n} from 'app/services/i18n/i18n';
+import {HelloWorld} from 'app/common/helloWorld/helloWorld';
 
 @Component({
   selector: 'home',
-  directives: [...FORM_DIRECTIVES],
+  directives: [HelloWorld],
   pipes: [],
   styles: [require('./home.style.scss')],
   template: require('./home.template.html')
